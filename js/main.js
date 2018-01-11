4aa447d209e3c9bf0f0eabf5251c1fb7ccc2157a
mpotts0201 <mpotts0201@gmail.com>
2018-01-11 10:40:14 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -15,9 +36,11 @@ var checkForMatch = function(){
 }
 
 var flipCard = function(cardId){
-    var card = cards[cardId];
+    var card = cards[cardId].rank;
     cardsInPlay.push(card);
     console.log("User flipped " + card);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 
     checkForMatch();
 }
